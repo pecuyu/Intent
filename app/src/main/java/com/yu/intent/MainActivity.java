e5521d0e79e874e4c9bed032e60d3650afa1b7c4
unknown <D22436@SZCKT.COM>
2017-07-27 18:32:49 +0800

    add button click

--- app/src/main/java/com/yu/intent/MainActivity.java
+++ app/src/main/java/com/yu/intent/MainActivity.java
@@ -1,13 +1,43 @@
 package com.yu.intent;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        startActivityExplicitly();
+    }
+
+
+    public void explicitClick(View view) {
+        startActivityExplicitly();
+    }
+
+    public void implicitClick(View view) {
+        startActivityImplicitly();
+    }
+
+
+
+    // implicit intent 隐式意图
+    public void startActivityImplicitly() {
+
+    }
+
+
+
+    // explicit intent 显式意图
+    public void startActivityExplicitly() {
+        Intent intent = new Intent();
+        intent.setClass(this, SecondActivity.class);
+        startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/com/yu/intent/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.yu.intent;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * Created by D22436 on 2017/7/27.
+ */
+
+public class SecondActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
+
+
